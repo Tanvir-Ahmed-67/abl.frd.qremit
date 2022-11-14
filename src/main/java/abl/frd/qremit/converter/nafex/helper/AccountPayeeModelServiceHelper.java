9fb1fac37428e1549e15a7c823050d4b3dbce8d3
Tanvir-Ahmed-67 <tanvirahmed67@yahoo.com>
2022-11-14 13:08:51 +0600

    initial commit

--- src/main/java/abl/frd/qremit/converter/nafex/helper/AccountPayeeModelServiceHelper.java
+++ src/main/java/abl/frd/qremit/converter/nafex/helper/AccountPayeeModelServiceHelper.java
@@ -3,18 +3,27 @@ package abl.frd.qremit.converter.nafex.helper;
 import abl.frd.qremit.converter.nafex.model.AccountPayeeModel;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
-import org.apache.commons.csv.QuoteMode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.DecimalFormat;
 import java.util.Arrays;
 import java.util.List;
 
 @Component
 public class AccountPayeeModelServiceHelper {
+    private static final DecimalFormat df = new DecimalFormat("0.00");
+    private static float incentivePercentage;
+    @Autowired
+    public AccountPayeeModelServiceHelper(@Value("${incentive.percentage}") float incentivePercentage) {
+        this.incentivePercentage = incentivePercentage;
+    }
+
     public static ByteArrayInputStream AccountPayeeModelToCSV(List<AccountPayeeModel> accountPayeeModelList) {
         final CSVFormat format = CSVFormat.DEFAULT;
 
@@ -41,7 +50,7 @@ public class AccountPayeeModelServiceHelper {
                         "PRINCIPAL CORP.BR.",
                         "22",
                         "1",
-                        "0000", //incentive
+                        calculatePercentage(accountPayeeModel.getAmount()), //incentive
                         "15"
                 );
                 csvPrinter.printRecord(data);
@@ -51,5 +60,11 @@ public class AccountPayeeModelServiceHelper {
         } catch (IOException e) {
             throw new RuntimeException("fail to Download " + e.getMessage());
         }
+    }
+
+    public static Double calculatePercentage(Double mainAmount){
+        Double percentage;
+        percentage = (incentivePercentage / 100f) * mainAmount;
+        return Double.valueOf(df.format(percentage));
     }
 }
