853e3e9af4b7ad4bd06149d69659742c963472ce
Tanvir-Ahmed-67 <tanvirahmed67@yahoo.com>
2023-10-02 12:20:09 +0600

    initial commit

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,20 +1,20 @@
 function downloadCoc(file, coc) {
-    window.location="/qremit/downloadcoc/"+file+"/"+coc;
+    window.location="/downloadcoc/"+file+"/"+coc;
 
 }
 function downloadAccountPayee(file, accountpayee) {
-    window.location="/qremit/downloadaccountpayee/"+file+"/"+accountpayee;
+    window.location="/downloadaccountpayee/"+file+"/"+accountpayee;
 
 }
 function downloadOnline(file, online) {
-    window.location="/qremit/downloadonline/"+file+"/"+online;
+    window.location="/downloadonline/"+file+"/"+online;
 
 }
 function downloadBeftn(file, beftn) {
-    window.location="/qremit/downloadbeftn/"+file+"/"+beftn;
+    window.location="/downloadbeftn/"+file+"/"+beftn;
 
 }
 
 function  downloadBeftnIncentive(file, beftn){
-    window.location="/qremit/downloadBeftnIncentive/"+file+"/"+beftn;
+    window.location="/downloadBeftnIncentive/"+file+"/"+beftn;
 }
