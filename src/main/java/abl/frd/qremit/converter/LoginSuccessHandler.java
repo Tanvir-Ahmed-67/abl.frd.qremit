c53802cb22065ab78c739b34f46040917f877a64
Tanvir-Ahmed-67 <tanvirahmed67@yahoo.com>
2023-10-04 10:37:13 +0600

    initial commit

--- src/main/java/abl/frd/qremit/converter/LoginSuccessHandler.java
+++ src/main/java/abl/frd/qremit/converter/LoginSuccessHandler.java
@@ -2,7 +2,6 @@ package abl.frd.qremit.converter;
 
 import abl.frd.qremit.converter.nafex.helper.MyUserDetails;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -10,7 +9,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Collection;
 
 @Component
 public class LoginSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
