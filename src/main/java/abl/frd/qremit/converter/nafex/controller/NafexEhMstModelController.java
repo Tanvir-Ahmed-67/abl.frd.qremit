eb8c8e95655afa7842d220abcfde4b8b57c1d8c2
Tanvir-Ahmed-67 <tanvirahmed67@yahoo.com>
2023-10-31 15:30:59 +0600

    html page renamed

--- src/main/java/abl/frd/qremit/converter/nafex/controller/NafexEhMstModelController.java
+++ src/main/java/abl/frd/qremit/converter/nafex/controller/NafexEhMstModelController.java
@@ -5,7 +5,6 @@ import abl.frd.qremit.converter.nafex.service.NafexModelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,7 +24,7 @@ public class NafexEhMstModelController {
     }
     @RequestMapping("/home")
     public String loginSubmit(){
-        return "/pages/landingpage";
+        return "/pages/adminLandingPage";
     }
     @RequestMapping("/logout")
     public String logoutSuccessPage(){
