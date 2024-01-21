19e28fb59290e3327f9e7c5d94d376a18eb0a3f2
Tanvir-Ahmed-67 <tanvirahmed67@yahoo.com>
2024-01-21 12:52:07 +0600

    multiple nrta code selection processing

--- src/main/java/abl/frd/qremit/converter/nafex/repository/AccountPayeeModelRepository.java
+++ src/main/java/abl/frd/qremit/converter/nafex/repository/AccountPayeeModelRepository.java
@@ -1,11 +1,9 @@
 package abl.frd.qremit.converter.nafex.repository;
 
 import abl.frd.qremit.converter.nafex.model.AccountPayeeModel;
-import abl.frd.qremit.converter.nafex.model.CocModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 @Repository
