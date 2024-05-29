b4d32dba75c7dce43c01f619c5c618b4cdc0ffda
ashikur-rahman <shuvo133@gmail.com>
2024-05-29 12:53:00 +0600

    User sidebar menu and links

--- src/main/java/abl/frd/qremit/converter/nafex/repository/UserModelRepository.java
+++ src/main/java/abl/frd/qremit/converter/nafex/repository/UserModelRepository.java
@@ -9,6 +9,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
+import java.util.Map;
 @Repository
 public interface UserModelRepository extends JpaRepository<User, Integer> {
     @Query("SELECT u FROM User u WHERE u.userName = :username")
@@ -17,9 +18,16 @@ public interface UserModelRepository extends JpaRepository<User, Integer> {
     // @Query("SELECT GROUP_CONCAT(c.exchangeName) AS exchange_name FROM User u JOIN ExchangeHouseModel c ON FIND_IN_SET(c.exchangeCode, REPLACE(u.exchangeCode, ',', ',')) > 0 WHERE u.id = :userId GROUP BY u.id")
     // public String findExchangeNamesByUserId(@Param("userId") int userId);
 
-    @Query(value = "SELECT GROUP_CONCAT(c.exchange_name) AS exchange_name FROM user u JOIN ex_house_list c ON FIND_IN_SET(c.exchange_code, u.exchange_code) WHERE u.user_id = :userId GROUP BY u.user_id", nativeQuery = true)
-    public String findExchangeNamesByUserId(@Param("userId") int userId);
+    // @Query(value = "SELECT GROUP_CONCAT(c.exchange_name) AS exchange_name FROM user u JOIN ex_house_list c ON FIND_IN_SET(c.exchange_code, u.exchange_code) WHERE u.user_id = :userId GROUP BY u.user_id", nativeQuery = true)
+    // public String findExchangeNamesByUserId(@Param("userId") int userId);
    
+
+    // @Query(value = "SELECT GROUP_CONCAT(c.exchange_short_name) AS exchange_short_name FROM user u JOIN ex_house_list c ON FIND_IN_SET(c.exchange_code, u.exchange_code) WHERE u.user_id = :userId GROUP BY u.user_id", nativeQuery = true)
+    // public String findExchangeNamesControllerByUserId(@Param("userId") int userId);
+
+    @Query(value = "SELECT GROUP_CONCAT(c.exchange_short_name) AS exchange_short_name, GROUP_CONCAT(c.exchange_name) AS exchange_name FROM user u JOIN ex_house_list c ON FIND_IN_SET(c.exchange_code, u.exchange_code) WHERE u.user_id = :userId GROUP BY u.user_id", nativeQuery = true)
+    Map<String, String> findExchangeNamesByUserId(@Param("userId") int userId);
+
     // @Query(value = "SELECT GROUP_CONCAT(c.exchangeName) AS exchange_name FROM User u JOIN ExchangeHouseModel c ON FIND_IN_SET(c.exchangeCode, u.exchangeCode) WHERE u.id = :userId GROUP BY u.id")
     // public String findExchangeNamesByUserId(@Param("userId") int userId);  
 
