dab9888f9d536c737db9259c3b7d494029cdb662
bhaktear <bhaktear@gmail.com>
2024-06-03 15:20:52 +0600

    new file upload

--- src/main/java/abl/frd/qremit/converter/nafex/controller/BecModelController.java
+++ src/main/java/abl/frd/qremit/converter/nafex/controller/BecModelController.java
@@ -1,24 +1,29 @@
 package abl.frd.qremit.converter.nafex.controller;
 import abl.frd.qremit.converter.nafex.helper.BecModelServiceHelper;
+import abl.frd.qremit.converter.nafex.helper.MyUserDetails;
 import abl.frd.qremit.converter.nafex.model.FileInfoModel;
 import abl.frd.qremit.converter.nafex.service.BecModelService;
+import abl.frd.qremit.converter.nafex.service.MyUserDetailsService;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 
 @Controller
-@RequestMapping("/bec")
+//@RequestMapping("/bec")
 public class BecModelController {
     private final BecModelService becModelService;
-
+    private final MyUserDetailsService myUserDetailsService;
+    
     @Autowired
-    public BecModelController(BecModelService becModelService){
+    public BecModelController(BecModelService becModelService,MyUserDetailsService myUserDetailsService){
+        this.myUserDetailsService = myUserDetailsService;
         this.becModelService = becModelService;
     }
 
@@ -28,8 +33,11 @@ public class BecModelController {
         System.out.println("Test");
         return "bec_home";
     }
-    @PostMapping("/upload")
-    public String uploadFile(@RequestParam("file") MultipartFile file, Model model) {
+    
+    @PostMapping("/becUpload")
+    public String uploadFile(@AuthenticationPrincipal MyUserDetails userDetails, @ModelAttribute("file") MultipartFile file, Model model) {
+        model.addAttribute("exchangeMap", myUserDetailsService.getLoggedInUserMenu(userDetails));  
+
         String message = "";
         String count ="";
         FileInfoModel fileInfoModelObject;
@@ -38,16 +46,15 @@ public class BecModelController {
             try {
                 fileInfoModelObject = becModelService.save(file);
                 model.addAttribute("fileInfo", fileInfoModelObject);
-                return "downloadPage";
-
+                return "/pages/user/userUploadSuccessPage";
             } catch (Exception e) {
                 e.printStackTrace();
                 message = "Could not upload the file: " + file.getOriginalFilename() + "!";
-                return "downloadPage";
+                return "/pages/user/userUploadSuccessPage";
             }
         }
         message = "Please upload a csv file!";
-        return "downloadPage";
+        return "/pages/user/userUploadSuccessPage";
     }
 
 }
---
+++ src/main/java/abl/frd/qremit/converter/nafex/controller/UtilsController.java
@@ -0,0 +1,46 @@
+package abl.frd.qremit.converter.nafex.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import abl.frd.qremit.converter.nafex.service.MyUserDetailsService;
+import abl.frd.qremit.converter.nafex.helper.MyUserDetails;
+
+
+@Controller
+@RequestMapping("/utils")
+public class UtilsController {
+    private final MyUserDetailsService myUserDetailsService;
+    public UtilsController(MyUserDetailsService myUserDetailsService){
+        this.myUserDetailsService = myUserDetailsService;
+    }
+    
+    @GetMapping("/index")
+    public String index(@AuthenticationPrincipal MyUserDetails userDetails, Model model){
+        model.addAttribute("exchangeMap", myUserDetailsService.getLoggedInUserMenu(userDetails));
+        return "/layouts/dashboard";
+    }
+    @PostMapping("/upload")
+    public String uploadFile(@AuthenticationPrincipal MyUserDetails userDetails,@RequestParam("file") MultipartFile file,@RequestParam("exName") String exName, Model model){
+        model.addAttribute("exchangeMap", myUserDetailsService.getLoggedInUserMenu(userDetails));
+
+        model.addAttribute("exName", exName);
+        model.addAttribute("file", file);
+        
+        String redirectUrl ="";
+        switch(exName){
+            case "nafex":
+                redirectUrl  = "/nafexUpload";
+                break;
+            case "bec":
+                redirectUrl  = "/becUpload";
+                break;
+        }
+        return "forward:" + redirectUrl;
+    }
+}
