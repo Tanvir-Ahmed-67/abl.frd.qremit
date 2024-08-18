496e9183f7db6b8cfae6cbd8a4dcd7e9ff21ca1e
Tanvir-Ahmed-67 <tanvirahmed67@yahoo.com>
2024-08-18 16:02:29 +0600

    Api beftn table to dynamic table insertion done.

--- src/main/java/abl/frd/qremit/converter/nafex/service/DynamicTableService.java
+++ src/main/java/abl/frd/qremit/converter/nafex/service/DynamicTableService.java
@@ -1,12 +1,14 @@
 package abl.frd.qremit.converter.nafex.service;
 
-import abl.frd.qremit.converter.nafex.model.ApiBeftnModel;
-import abl.frd.qremit.converter.nafex.model.ExchangeHouseModel;
+import abl.frd.qremit.converter.nafex.helper.RepositoryModelWrapper;
+import abl.frd.qremit.converter.nafex.model.*;
 import abl.frd.qremit.converter.nafex.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
+
+import java.lang.reflect.Constructor;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
@@ -20,69 +22,47 @@ public class DynamicTableService {
     UserModelRepository userModelRepository;
     @Autowired
     private ApiBeftnModelRepository apiBeftnModelRepository;
-    @Autowired
-    private ApiT24ModelRepository apiT24ModelRepository;
 
     LocalDateTime currentDateTime = LocalDateTime.now();
-    private final Map<String, JpaRepository> repositoryInstanceMap = new HashMap<>();
-    private final ApplicationContext applicationContext;
+    private Map<String, RepositoryModelWrapper<?>> repositoryModelMap = new HashMap<>();
+
+
+    @Bean
+    public Map<String, RepositoryModelWrapper<?>> repositoryModelMap(
+            AgexSingaporeModelRepository agexSingaporeModelRepository,
+            BecModelRepository becModelRepository,
+            EzRemitModelRepository ezRemitModelRepository,
+            MuzainiModelRepository muzainiModelRepository,
+            NafexModelRepository nafexModelRepository,
+            RiaModelRepository riaModelRepository) {
+        repositoryModelMap.put("7010226", new RepositoryModelWrapper<>(agexSingaporeModelRepository, AgexSingaporeModel.class));
+        repositoryModelMap.put("7010209", new RepositoryModelWrapper<>(becModelRepository, BecModel.class));
+        repositoryModelMap.put("7010299", new RepositoryModelWrapper<>(ezRemitModelRepository, EzRemitModel.class));
+        repositoryModelMap.put("7010231", new RepositoryModelWrapper<>(muzainiModelRepository, MuzainiModel.class));
+        repositoryModelMap.put("7010234", new RepositoryModelWrapper<>(nafexModelRepository, NafexEhMstModel.class));
+        repositoryModelMap.put("7010290", new RepositoryModelWrapper<>(riaModelRepository, RiaModel.class));
+        return repositoryModelMap;
+    }
 
     public void transferData() {
-        DoSomething();
         List<ApiBeftnModel> allRows = apiBeftnModelRepository.findAll();
         for (ApiBeftnModel row : allRows) {
-            String ExchangeCode = row.getExchangeCode();
-            JpaRepository<?, Long> repository = repositoryInstanceMap.get(ExchangeCode);
-            if (repository != null) {
-                try {
-                    Class<?> entityClass = repository.getClass().getDeclaredMethod("getDomainClass").invoke(repository).getClass();
-                    Object entityInstance = entityClass.getDeclaredConstructor().newInstance();
-                    entityClass.getMethod("setData", String.class).invoke(entityInstance, row);
-                    // Cast and save the entity instance
-                    saveEntity(repository, entityInstance);
-                } catch (Exception e) {
-                    // Handle exception
-                    e.printStackTrace();
-                }
-            } else {
-                // Handle case where repository is not found for a code
-                System.out.println("No repository found for code: " + ExchangeCode);
-            }
-        }
-    }
-
-    private <T> void saveEntity(JpaRepository<T, Long> repository, Object entityInstance) {
-        T entity = (T) entityInstance; // Cast the entity to the correct type
-        repository.save(entity); // Save the entity
-    }
-
-    @Autowired
-    public DynamicTableService(ApplicationContext applicationContext) {
-        this.applicationContext = applicationContext;
-    }
-
-    public Map<String, JpaRepository> DoSomething() {
-        List<ExchangeHouseModel> exchangeHouseModelList = exchangeHouseModelRepository.findAllActiveExchangeHouseList();
-        String modelClassName;
-        String exchangeCode;
-        for (ExchangeHouseModel exchangeHouseModel : exchangeHouseModelList) {
             try {
-                if (exchangeHouseModel.getExchangeCode().equals("710000") || exchangeHouseModel.getExchangeCode().equals("720000")) {
-                    continue;
+                String exchangeCode = row.getExchangeCode();
+                RepositoryModelWrapper<?> wrapper = repositoryModelMap.get(exchangeCode);
+                if (wrapper != null) {
+                    JpaRepository repository = wrapper.getRepository();
+                    Class<?> modelClass = wrapper.getModelClass();
+                    Constructor<?> constructor = modelClass.getConstructor(String.class, String.class, String.class, Double.class, String.class, String.class, String.class, String.class, String.class, String.class, String.class, String.class, String.class, String.class, String.class, String.class, String.class, String.class, String.class, String.class, String.class, String.class, String.class, String.class, String.class, String.class, String.class, FileInfoModel.class, User.class);
+                    Object modelInstance = constructor.newInstance(row.getExchangeCode(), row.getTransactionNo(), row.getCurrency(), row.getAmount(), row.getEnteredDate(), row.getRemitterName(), row.getRemitterMobile(), row.getBeneficiaryName(), row.getBeneficiaryAccount(), row.getBeneficiaryMobile(), row.getBankName(), row.getBankCode(), row.getBranchName(), row.getBranchCode(), row.getDraweeBranchName(), row.getDraweeBranchCode(), row.getPurposeOfRemittance(), row.getSourceOfIncome(), row.getProcessFlag(), row.getTypeFlag(), row.getProcessedBy(), row.getProcessedDate(), row.getExtraC(), row.getCheckT24(), row.getCheckCoc(), row.getCheckAccPayee(), row.getCheckBeftn(), row.getFileInfoModel(), row.getUserModel());
+                    repository.save(modelInstance);
+                } else {
+                    throw new IllegalArgumentException("No repository or model class found for cxchangeCode: " + exchangeCode);
                 }
-                modelClassName = exchangeHouseModel.getModelClassName();
-                exchangeCode = exchangeHouseModel.getExchangeCode();
-                String repositoryClassName = modelClassName;
-                Class<?> repositoryClass = Class.forName("abl.frd.qremit.converter.nafex.repository." + repositoryClassName);
-                JpaRepository repository = (JpaRepository) applicationContext.getBean(repositoryClass);
-                if (repository == null) {
-                    throw new IllegalArgumentException("No repository found for bean name: " + repositoryClass);
-                }
-                this.repositoryInstanceMap.put(exchangeCode, repository);
             } catch (Exception e) {
-                throw new RuntimeException("Failed to initialize repository: " + e.getMessage());
+                // Handle exception
+                e.printStackTrace();
             }
         }
-        return repositoryInstanceMap;
     }
 }
