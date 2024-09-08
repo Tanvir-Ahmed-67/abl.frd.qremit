0ca0825cd8a93ef2abc4740488c40cb3bff1d440
bhaktear <bhaktear@gmail.com>
2024-09-08 09:57:14 +0600

    ErrorModel old

--- src/main/java/abl/frd/qremit/converter/nafex/service/LogModelService.java
+++ src/main/java/abl/frd/qremit/converter/nafex/service/LogModelService.java
@@ -1,7 +1,10 @@
 package abl.frd.qremit.converter.nafex.service;
-
+import java.util.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import abl.frd.qremit.converter.nafex.model.LogModel;
 import abl.frd.qremit.converter.nafex.repository.LogModelRepository;
 
@@ -12,10 +15,19 @@ public class LogModelService {
 
     public void findLogModelByErrorDataId(String errorDataId){
         LogModel logModel = logModelRepository.findByErrorDataId(errorDataId);
+        List<HashMap<String, Object>> dataList = processInfoData(logModel.getInfo());
 
     }
 
-    public void processInfoData(){
-        
+    public List<HashMap<String, Object>> processInfoData(String infoJson){
+        List<HashMap<String, Object>> dataList = new ArrayList<>();
+        ObjectMapper objectMapper = new ObjectMapper();
+        // Parse the JSON string into a Map
+        /* 
+        Map<String, Object> infoMap = objectMapper.readValue(infoJson, HashMap.class);
+        dataList.add((HashMap<String, Object>) infoMap.get("oldData"));
+        dataList.add((HashMap<String, Object>) infoMap.get("updatedData"));
+        */
+        return dataList;
     }
 }
