096c08a2d72e30ac226d83b901d1d694697cda30
bhaktear <bhaktear@gmail.com>
2024-10-20 16:26:47 +0600

    Singapore EzRemit code updated

--- src/main/java/abl/frd/qremit/converter/nafex/repository/EzRemitModelRepository.java
+++ src/main/java/abl/frd/qremit/converter/nafex/repository/EzRemitModelRepository.java
@@ -1,11 +1,8 @@
 package abl.frd.qremit.converter.nafex.repository;
-
-import abl.frd.qremit.converter.nafex.model.AgexSingaporeModel;
 import abl.frd.qremit.converter.nafex.model.EzRemitModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
+import java.util.*;
 
 @Repository
 public interface EzRemitModelRepository extends JpaRepository<EzRemitModel, Integer> {
