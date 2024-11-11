20b8fc3e56d9717d13c617871ed03a2ac6881e19
bhaktear <bhaktear@gmail.com>
2024-11-11 10:36:15 +0600

    AlBilad Service has updated and converter has completed

--- src/main/java/abl/frd/qremit/converter/nafex/service/AlBiladModelService.java
+++ src/main/java/abl/frd/qremit/converter/nafex/service/AlBiladModelService.java
@@ -53,7 +53,7 @@ public class AlBiladModelService {
             fileInfoModel.setExchangeCode(exchangeCode);
             fileInfoModel.setFileName(file.getOriginalFilename());
             fileInfoModel.setUploadDateTime(currentDateTime);
-            //fileInfoModelRepository.save(fileInfoModel);
+            fileInfoModelRepository.save(fileInfoModel);
 
             Map<String, Object> alBiladData = csvToAlBiladModels(file.getInputStream(), user, fileInfoModel, exchangeCode, nrtaCode, currentDateTime);
             List<AlBiladModel> alBiladModels = (List<AlBiladModel>) alBiladData.get("alBiladModelList");
@@ -107,63 +107,18 @@ public class AlBiladModelService {
             int i = 0;
             int duplicateCount = 0;
             while ((line = fileReader.readLine()) != null) {
-                if(line.startsWith("H") || line.startsWith("F"))    continue;
-                if(line.startsWith("D")){
-                    i++;
-                    Map<String, Object> data = processData(line);
-                    System.out.println(data);
-                }
-            }
-        }catch(IOException e){
-            String message = "fail to store csv data: " + e.getMessage();
-            resp.put("errorMessage", message);
-            throw new RuntimeException(message);
-        }
-        return resp;
-    }
-
-    public Map<String, Object> processData(String line){
-        Map<String, Object> data = new HashMap<>();
-        String transactionNo = line.substring(7,23).trim();
-        String enteredDate = line.substring(23,31).trim();
-        Double amount = Double.parseDouble(line.substring(31, 49).trim());
-        String currency = line.substring(49, 52).trim();
-        String remitterName = line.substring(55, 90).trim();
-        
-
-        data.put("transactionNo", transactionNo);
-        data.put("enteredDate", enteredDate);
-        data.put("amount", String.valueOf(amount));
-        data.put("currency", currency);
-        data.put("remitterName", remitterName);
-        return data;
-    }
-
-    /*
-    public Map<String, Object> csvToAlBiladModels(InputStream is, User user, FileInfoModel fileInfoModel, String exchangeCode, String nrtaCode, LocalDateTime currentDateTime){
-        Map<String, Object> resp = new HashMap<>();
-        Optional<AlBiladModel> duplicateData;
-        try (BufferedReader fileReader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
-            CSVParser csvParser = new CSVParser(fileReader, CSVFormat.newFormat('|') .withIgnoreHeaderCase().withTrim())) {
-            Iterable<CSVRecord> csvRecords = csvParser.getRecords();
-            List<AlBiladModel> alBiladModelList = new ArrayList<>();
-            List<ErrorDataModel> errorDataModelList = new ArrayList<>();
-            List<String> transactionList = new ArrayList<>();
-            String duplicateMessage = "";
-            int i = 0;
-            int duplicateCount = 0;
-            for (CSVRecord csvRecord : csvRecords) {
-                if(firstRow.startsWith("H") || firstRow.startsWith("F"))    continue;
+                if(!line.startsWith("D"))   continue;
                 i++;
-                Map<String, Object> data = getCsvData(csvRecord);
-                String transactionNo = csvRecord.get(1).trim();
-                String amount = csvRecord.get(3).trim();
+                Map<String, Object> data = processData(line, exchangeCode);
+                //System.out.println(data);
+                String transactionNo = data.get("transactionNo").toString();
+                String amount = data.get("amount").toString();
                 duplicateData = alBiladModelRepository.findByTransactionNoIgnoreCaseAndAmountAndExchangeCode(transactionNo, CommonService.convertStringToDouble(amount), exchangeCode);
-                String beneficiaryAccount = csvRecord.get(7).trim();
-                String bankName = csvRecord.get(8).trim();
-                String branchCode = CommonService.fixRoutingNo(csvRecord.get(11).trim());
+                String beneficiaryAccount = data.get("beneficiaryAccount").toString();
+                String bankName = data.get("bankName").toString();
+                String branchCode = CommonService.fixRoutingNo(data.get("branchCode").toString());
                 
-                Map<String, Object> errResp = CommonService.checkError(data, errorDataModelList, nrtaCode, fileInfoModel, user, currentDateTime, csvRecord.get(0).trim(), duplicateData, transactionList);
+                Map<String, Object> errResp = CommonService.checkError(data, errorDataModelList, nrtaCode, fileInfoModel, user, currentDateTime, nrtaCode, duplicateData, transactionList);
                 if((Integer) errResp.get("err") == 1){
                     errorDataModelList = (List<ErrorDataModel>) errResp.get("errorDataModelList");
                     continue;
@@ -188,22 +143,22 @@ public class AlBiladModelService {
                 alBiladModel.setUploadDateTime(currentDateTime);
                 alBiladModelList.add(alBiladModel);
             }
-           //save error data
-           Map<String, Object> saveError = errorDataModelService.saveErrorModelList(errorDataModelList);
-           if(saveError.containsKey("errorCount")) resp.put("errorCount", saveError.get("errorCount"));
-           if(saveError.containsKey("errorMessage")){
-               resp.put("errorMessage", saveError.get("errorMessage"));
-               return resp;
-           }
-           //if both model is empty then delete fileInfoModel
-           if(errorDataModelList.isEmpty() && alBiladModelList.isEmpty()){
-               fileInfoModelService.deleteFileInfoModelById(fileInfoModel.getId());
-           }
-           resp.put("alBiladModelList", alBiladModelList);
-           if(!resp.containsKey("errorMessage")){
+            //save error data
+            Map<String, Object> saveError = errorDataModelService.saveErrorModelList(errorDataModelList);
+            if(saveError.containsKey("errorCount")) resp.put("errorCount", saveError.get("errorCount"));
+            if(saveError.containsKey("errorMessage")){
+                resp.put("errorMessage", saveError.get("errorMessage"));
+                return resp;
+            }
+            //if both model is empty then delete fileInfoModel
+            if(errorDataModelList.isEmpty() && alBiladModelList.isEmpty()){
+                fileInfoModelService.deleteFileInfoModelById(fileInfoModel.getId());
+            }
+            resp.put("alBiladModelList", alBiladModelList);
+            if(!resp.containsKey("errorMessage")){
                 resp.put("errorMessage", CommonService.setErrorMessage(duplicateMessage, duplicateCount, i));
             }
-        } catch (IOException e) {
+        }catch(IOException e){
             String message = "fail to store csv data: " + e.getMessage();
             resp.put("errorMessage", message);
             throw new RuntimeException(message);
@@ -211,31 +166,68 @@ public class AlBiladModelService {
         return resp;
     }
 
-
-    public Map<String, Object> getCsvData(CSVRecord csvRecord, String exchangeCode, String transactionNo, String beneficiaryAccount, String bankName, String branchCode){
+    public Map<String, Object> processData(String line, String exchangeCode){
         Map<String, Object> data = new HashMap<>();
+        String transactionNo = line.substring(7,23).trim();
+        String enteredDate = line.substring(23,31).trim();
+        Double amount = Double.parseDouble(line.substring(31, 49).trim());
+        String currency = line.substring(49, 52).trim();
+        String remitterName = line.substring(55, 90).trim();
+        String branchName = line.substring(230, 265).trim();
+        String branchCode = line.substring(265, 300).trim();
+        String bankStr = line.substring(300,335).trim();
+        String branch2 = line.substring(580, 615).trim();
+        Map<String, Object> bank = getBankDetails(bankStr,branchName, branchCode, branch2);
+        String beneficiaryName = line.substring(440, 545).trim();
+        String beneficiaryAccount = line.substring(545, 580).trim();
+        String sourceOfIncome = line.substring(755, 790).trim();
+
         data.put("exchangeCode", exchangeCode);
         data.put("transactionNo", transactionNo);
-        data.put("currency", csvRecord.get(2));
-        data.put("amount", csvRecord.get(3));
-        data.put("enteredDate", csvRecord.get(4));
-        data.put("remitterName", csvRecord.get(5));
-        data.put("remitterMobile", csvRecord.get(17));
-        data.put("beneficiaryName", csvRecord.get(6));
-        data.put("beneficiaryAccount", beneficiaryAccount);
-        data.put("beneficiaryMobile", csvRecord.get(12));
-        data.put("bankName", bankName);
-        data.put("bankCode", csvRecord.get(9));
-        data.put("branchName", csvRecord.get(10));
+        data.put("enteredDate", enteredDate);
+        data.put("amount", String.valueOf(amount));
+        data.put("currency", currency);
+        data.put("remitterName", remitterName);
+        data.put("branchName", bank.get("branchName").toString());
         data.put("branchCode", branchCode);
-        data.put("draweeBranchName", csvRecord.get(13));
-        data.put("draweeBranchCode", csvRecord.get(14));
-        data.put("purposeOfRemittance", csvRecord.get(15));
-        data.put("sourceOfIncome", csvRecord.get(16));
+        data.put("bankName", bank.get("bankName").toString());
+        data.put("bankCode", bank.get("bankCode").toString());
+        data.put("beneficiaryName", beneficiaryName);
+        data.put("beneficiaryAccount", beneficiaryAccount);
+        data.put("remitterMobile", "");
+        data.put("beneficiaryMobile","");
+        data.put("draweeBranchName", "");
+        data.put("draweeBranchCode", "");
+        data.put("purposeOfRemittance", "");
+        data.put("sourceOfIncome", sourceOfIncome);
         data.put("processFlag", "");
         data.put("processedBy", "");
         data.put("processedDate", "");
         return data;
     }
-        */
+
+    public Map<String, Object> getBankDetails(String bankStr, String branchName, String branchCode, String branch2){
+        Map<String, Object> resp = new HashMap<>();
+        String bankName = "Agrani Bank";
+        String bankCode = "11";
+        if(!bankStr.isEmpty() && !CommonService.checkAgraniBankName(bankStr)){
+            bankName = bankStr;
+            bankCode = "";
+        }
+        branchName = getBranchName(branchName, branch2);
+        resp.put("bankName", bankName);
+        resp.put("bankCode", bankCode);
+        resp.put("branchName", branchName);
+        return resp;
+    }
+
+    public String getBranchName(String branchName, String branch2){
+        String search = "remittance-to-3rd-party-bank";
+        String search2 = "remittance to 3rd party bank";
+        if(branchName.toLowerCase().contains(search) || branchName.toLowerCase().contains(search2)){
+            branchName = branch2;
+        }
+        return branchName;
+    }
+
 }
