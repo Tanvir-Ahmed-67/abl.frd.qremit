d5d85a912108bb7225519afe21995d4c98a22204
Tanvir-Ahmed-67 <tanvirahmed67@yahoo.com>
2025-01-02 16:55:28 +0600

    Daily Reimbursement Refactored.

--- src/main/resources/static/js/reimbursement.js
+++ src/main/resources/static/js/reimbursement.js
@@ -1,16 +1,18 @@
-$(document).ready(function (){
-    $(document).off('change','#report_date');
-    $(document).on('change','#report_date', function(e){
+$(document).ready(function () {
+    var tbl = "#remimbursement_tbl";
+    $('#showDataButton').on('click', function (e){
         e.preventDefault();
-        var val = $(this).val();
-        if(!val)    return false;
-        var url = "/showDailyReimbursement?date=" + val;
-        $.ajax({
-            url: url,
-        }).done(function (resp){
-            console.log(resp);
-        }).fail(function (){
-            alert("Error getting from server");
-        })
+        var fromDate = $("#report_date").val();
+        var toDate = $("#report_end_date").val();
+        get_remimbursement(fromDate, toDate);
+
     });
+    function get_remimbursement(fromDate, toDate){
+        var url = "/getReimbursementData?fromDate=" + fromDate + "&toDate=" + toDate;
+        var column = ['sl','exchangeCode','transactionNo','beneficiaryAccount','branchCode','branchName',
+            'mainAmount','govtIncentiveAmount','agraniIncentiveAmount','type'];
+        var cols = DataTableColumns(column);
+        $("#dataSection").show();
+        get_dynamic_dataTable(tbl, url, cols);
+    }
 });
