52ec9b60c4ca1be3c793bccf0bc2af37f3e69d90
bhaktear <bhaktear@gmail.com>
2025-01-12 16:01:30 +0600

    removed unused commented code in cocPaidModelController and Service

--- src/main/java/abl/frd/qremit/converter/service/CocPaidModelService.java
+++ src/main/java/abl/frd/qremit/converter/service/CocPaidModelService.java
@@ -77,34 +77,6 @@ public class CocPaidModelService {
                 resp.put("errorMessage", e.getMessage());
             }
 
-            /*
-            if(cocPaidModelList.size()!=0) {
-                int ind = 0;
-                for (CocPaidModel cocPaidModel : cocPaidModelList) {
-                    cocPaidModel.setFileInfoModel(fileInfoModel);
-                    cocPaidModel.setUserModel(user);
-                    if (ind == 0) {
-                        fileInfoModel.setExchangeCode(exchangeCode);
-                        ind++;
-                    }
-                }
-            }
-            else {
-                return null;
-            }
-            int totalCount = cocPaidModelList.size();
-            fileInfoModel.setCocPaidModelList(cocPaidModelList);
-            fileInfoModel.setFileName(file.getOriginalFilename());
-            fileInfoModel.setUploadDateTime(currentDateTime);
-            fileInfoModel.setIsSettlement(1);
-            fileInfoModel.setCocCount(String.valueOf(totalCount));
-            fileInfoModel.setAccountPayeeCount("0");
-            fileInfoModel.setOnlineCount("0");
-            fileInfoModel.setBeftnCount("0");
-            fileInfoModel.setTotalCount(String.valueOf(totalCount));
-            fileInfoModelRepository.save(fileInfoModel);
-            return fileInfoModel;
-            */
         }
         catch(Exception e){
             String message = "fail to store csv data: " + e.getMessage();
@@ -164,33 +136,6 @@ public class CocPaidModelService {
                 cocPaidModel.setUserModel(user);
                 cocPaidModelList.add(cocPaidModel);
                 totalAmount += amount;
-                /*
-                if (duplicateData.isPresent()){  // Checking Duplicate Transaction No in this block
-                    continue;
-                }
-                String routingNo = CommonService.fixRoutingNo(csvRecord.get(8));
-                Map<String, Object> routingMap = customQueryService.getRoutingDetailsByRoutingNo(routingNo);
-                
-                CocPaidModel cocPaidModel = new CocPaidModel(
-                        csvRecord.get(0), //exCode
-                        csvRecord.get(1), //Tranno
-                        CommonService.convertStringToDouble(csvRecord.get(4)), //Amount
-                        CommonService.convertStringToDate(csvRecord.get(3)), //Entered Date
-                        CommonService.convertStringToDate(csvRecord.get(11)), //Paid Date
-                        csvRecord.get(5), //remitter Name
-                        csvRecord.get(6), // beneficiary Name
-                        csvRecord.get(7), //beneficiaryAccount
-                        routingNo, //routingNo
-                        csvRecord.get(10), //beneficiary Mobile
-                        "Agrani Bank",
-                        "11",
-                        routingMap.get("branch_name").toString(),
-                        routingMap.get("abl_branch_code").toString(),// branch code have to put here
-                        csvRecord.get(12), //tr mode
-                        currentDateTime,    //uploadDateTime
-                        "4");  
-                cocPaidModelList.add(cocPaidModel);
-                */
             }
             //save error data
             Map<String, Object> saveError = errorDataModelService.saveErrorModelList(errorDataModelList);
