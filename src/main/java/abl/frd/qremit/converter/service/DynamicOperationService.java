7096dc30ccfa749afd439130f8cb03c19f30b53f
bhaktear <bhaktear@gmail.com>
2025-02-06 17:00:07 +0600

    updateFileInfoCount method added for code refaction in transferErrorData method

--- src/main/java/abl/frd/qremit/converter/service/DynamicOperationService.java
+++ src/main/java/abl/frd/qremit/converter/service/DynamicOperationService.java
@@ -279,26 +279,15 @@ public class DynamicOperationService {
                 int checkBeftn = (("3").equals(typeFlag))  ? 1:0;
                 int checkCoc = (("4").equals(typeFlag))  ? 1:0;
                 FileInfoModel fileInfoModel = fileInfoModelRepository.findById(fileInfoId);
-                Integer accPayeeCount = CommonService.convertStringToInt(fileInfoModel.getAccountPayeeCount()) + checkAccPayee;
-                Integer beftnCount = CommonService.convertStringToInt(fileInfoModel.getBeftnCount()) + checkBeftn;
-                Integer cocCount = CommonService.convertStringToInt(fileInfoModel.getCocCount()) + checkCoc;
-                Integer t24Count = CommonService.convertStringToInt(fileInfoModel.getOnlineCount()) + checkT24;
-                Integer totalCount = accPayeeCount + beftnCount + cocCount + t24Count;
-                fileInfoModel.setAccountPayeeCount(String.valueOf(accPayeeCount));
-                fileInfoModel.setBeftnCount(String.valueOf(beftnCount));
-                fileInfoModel.setCocCount(String.valueOf(cocCount));
-                fileInfoModel.setOnlineCount(String.valueOf(t24Count));
-                fileInfoModel.setTotalCount(String.valueOf(totalCount));
-                int erroCount = fileInfoModel.getErrorCount() - 1;
-                fileInfoModel.setErrorCount(erroCount);
-                User user = userModelRepository.findByUserId(userId);
-
+                fileInfoModel = updateFileInfoCount(fileInfoModel, checkT24, checkAccPayee, checkBeftn, checkCoc, 1);
+                User user = userModelRepository.findByUserId(userId);      
                 Object modelInstance = constructor.newInstance(exchangeCode, updatedData.get("transactionNo"), updatedData.get("currency"), amount, 
                     updatedData.get("enteredDate"), updatedData.get("remitterName"), updatedData.get("remitterMobile"), updatedData.get("beneficiaryName"), 
                     beneficiaryAccount, updatedData.get("beneficiaryMobile"), bankName, updatedData.get("bankCode"), 
                     updatedData.get("branchName"), branchCode, updatedData.get("draweeBranchName"), updatedData.get("draweeBranchCode"), 
                     updatedData.get("purposeOfRemittance"), updatedData.get("sourceOfIncome"), updatedData.get("processFlag"), typeFlag, 
                     updatedData.get("processedBy"), updatedData.get("processedDate"), currentDateTime, fileInfoModel, user);
+                
                 List<Object> modelInstanceList = new ArrayList<>();
                 modelInstanceList.add(modelInstance);
                 Map<String, Object> convertedDataModels = commonService.generateFourConvertedDataModel(modelInstanceList, fileInfoModel, user, currentDateTime, 0);
