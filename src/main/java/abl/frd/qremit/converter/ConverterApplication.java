d7eb92bfbb802519a65675e84b295b80f2d68896
bhaktear <bhaktear@gmail.com>
2025-04-28 16:54:47 +0600

    Temporary Report Cron Job has added

--- src/main/java/abl/frd/qremit/converter/ConverterApplication.java
+++ src/main/java/abl/frd/qremit/converter/ConverterApplication.java
@@ -1,8 +1,10 @@
 package abl.frd.qremit.converter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ConverterApplication {
 
 	public static void main(String[] args) {
