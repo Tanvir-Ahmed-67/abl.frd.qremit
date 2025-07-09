25e3d8723e075de327812b5a0bee9c97d4153cd9
bhaktear <bhaktear@gmail.com>
2025-07-09 15:19:31 +0600

    FileInfoModel Service & reopository getSettlementDataByExchangeCode method code refactored

--- src/main/java/abl/frd/qremit/converter/repository/FileInfoModelRepository.java
+++ src/main/java/abl/frd/qremit/converter/repository/FileInfoModelRepository.java
@@ -1,7 +1,5 @@
 package abl.frd.qremit.converter.repository;
-import abl.frd.qremit.converter.model.FileInfoModelDTO;
 import abl.frd.qremit.converter.model.FileInfoModel;
-
 import java.time.LocalDateTime;
 import java.util.*;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -23,8 +21,8 @@ public interface FileInfoModelRepository extends JpaRepository<FileInfoModel, In
     //void updateFileInfoModel(int id, String accountPayeeCount, String beftnCount, String cocCount, String onlineCount, String totalCount, int isSettlement, String unprocessedCount);
     @Query("SELECT n FROM FileInfoModel n WHERE n.uploadDateTime BETWEEN :startDate AND :endDate")
     List<FileInfoModel> getFileDetailsBetweenUploadedDate(@Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
-    @Query("SELECT new abl.frd.qremit.converter.model.FileInfoModelDTO(n, COUNT(n)) FROM FileInfoModel n WHERE n.exchangeCode= :exchangeCode AND n.isSettlement = :isSettlement AND n.uploadDateTime BETWEEN :startDate AND :endDate GROUP BY exchangeCode")
-    FileInfoModelDTO getSettlementDataByExchangeCode(@Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate, @Param("exchangeCode") String exchnageCode, @Param("isSettlement") int isSettlement);
+    @Query("SELECT n FROM FileInfoModel n WHERE n.exchangeCode= :exchangeCode AND n.isSettlement = :isSettlement AND n.uploadDateTime BETWEEN :startDate AND :endDate")
+    List<FileInfoModel> getSettlementDataByExchangeCode(@Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate, @Param("exchangeCode") String exchnageCode, @Param("isSettlement") int isSettlement);
     @Query("SELECT n FROM FileInfoModel n WHERE n.totalAmount IS NULL")
     List<FileInfoModel> getUploadedFileNotHavingTotalAmount();
     @Transactional
