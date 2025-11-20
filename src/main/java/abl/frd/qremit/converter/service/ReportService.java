608664971af6185a38358bafc9af3363ca82990f
bhaktear <bhaktear@gmail.com>
2025-11-20 17:00:51 +0600

    Routing Number Validation & Online, A/C Payee branch code validation has added

--- src/main/java/abl/frd/qremit/converter/service/ReportService.java
+++ src/main/java/abl/frd/qremit/converter/service/ReportService.java
@@ -62,6 +62,8 @@ public class ReportService {
     NpsbMfsService npsbMfsService;
     @Autowired
     BeftnReturnRepository beftnReturnRepository;
+    @Autowired
+    CustomQueryService customQueryService;
 
     DateTimeFormatter yyMMddFormatter = DateTimeFormatter.ofPattern("yyMMdd");     // YYMMDD
     DateTimeFormatter yyyyMMddFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd"); // YYYY/MM/DD
@@ -1081,9 +1083,18 @@ public class ReportService {
             return CommonService.getResp(1, errorMessage, null);
         }
         String typeFlag = CommonService.setTypeFlag(beneficiaryAccount, bankName, branchCode);
-        if(("2").equals(typeFlag)){
-            Map<String, Object> routingMap = commonService.checkAblBranchCode(branchCode);
-            if((Integer) routingMap.get("err") == 1)    return CommonService.getResp(1, "Invalid Branch Code for A/C Payee", null);
+        Map<String, Object> routingMap = new HashMap<>();
+        if(("2").equals(typeFlag) || ("1").equals(typeFlag)){
+            routingMap = commonService.checkAblBranchCode(branchCode);
+            if((Integer) routingMap.get("err") == 1)    return CommonService.getResp(1, "Invalid Branch Code for A/C Payee or Online", null);
+            List<Map<String, Object>> routingData = (List<Map<String, Object>>) routingMap.get("data");
+            formData.put("branchCode",routingData.get(0).get("abl_branch_code").toString());
+            formData.put("branchName",routingData.get(0).get("branch_name").toString());
+        }
+
+        if(("3").equals(typeFlag)){
+            routingMap = customQueryService.getRoutingDetails(branchCode, "");
+            if((Integer) routingMap.get("err") == 1)  return CommonService.getResp(1, "Invalid Routing No for BEFTN", null);
         }
         resp = dynamicOperationService.updateIndividualDataById(exchangeCode, fileInfoModel, user, transactionNo, formData, type, obj, typeFlag);
         if((Integer) resp.get("err") == 0){
